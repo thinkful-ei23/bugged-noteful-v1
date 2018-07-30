215b0cacadd04db89bafb140a84c6cca4de0ba96
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-07-30 15:08:09 -0400

    installed express

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 'use strict';
 
+const express = require("express");
+
+const app = express();
+
+
 // Load array of notes
 const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+
+app.listen(process.env.PORT, () =>
+  console.log(`Your app is listening on port ${process.env.PORT}`)
+);
