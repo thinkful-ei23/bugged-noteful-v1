1c591b9020fe3d964cfbd57d12ad94b17ddffb2a
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-07-30 17:05:27 -0400

    completed exercises

--- server.js
+++ server.js
@@ -9,7 +9,26 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-  res.json(data);
+  const search = req.query.searchTerm;
+  if (search) {
+    let searchResults = data.filter(item => item.title.includes(search));
+    res.json(searchResults);
+  } else {
+    res.json(data);
+  }
+});
+app.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  // let requestedData;
+  // for (let i = 0; i < data.length; i++) {
+  //   if (data[i].id === Number(id)) {
+  //     requestedData = data[i];
+  //   }
+  // }
+  const note = data.find(item => item.id === Number(id));
+  console.log(note);
+  res.json(note);
 });
 
 app.listen(8080, function () {
