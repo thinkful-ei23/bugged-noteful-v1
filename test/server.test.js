0d7f9e88e723cec456d3f0eee7cbf5ff8373bbe4
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-08-03 16:57:46 -0400

    added custom GET test on test/server.test

--- test/server.test.js
+++ test/server.test.js
@@ -16,3 +16,45 @@ describe('Reality check', function() {
     expect(2 + 2).to.equal(4);
   });
 });
+
+describe('Express static', function () {
+
+  it('GET request "/" should return the index page', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function (res) {
+        expect(res).to.exist;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
+
+describe('404 handler', function () {
+
+  it('should respond with 404 when given a bad path', function () {
+    return chai.request(app)
+      .get('/DOES/NOT/EXIST')
+      .then(res => {
+        expect(res).to.have.status(404);
+      });
+  });
+});
+
+describe('GET /api/notes', function() {
+  it('should return the default of 10 Notes as an array', function () {
+    return chai.request(app)
+      .get('/api/notes')
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("array");
+        expect(res.body.length).to.be.at.least(10);
+        const expectedKeys = ["id", "title", "content"];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object");
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+});
